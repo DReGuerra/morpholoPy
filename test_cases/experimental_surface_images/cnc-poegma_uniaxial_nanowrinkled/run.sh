d1056f386f6b9be87cdc5bd54767317902e1b400
Andre Guerra <andre.guerra@mail.mcgill.ca>
2025-04-26 12:50:59 -0400

    added run.sh

---
+++ test_cases/experimental_surface_images/cnc-poegma_uniaxial_nanowrinkled/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python src/characteristic_length.py \
+--file kdf_uniaxial_20um.tif \
+--dconv True \
+--pop_num 1 \
+--bar_len 20 \
+--dof_lo_sigma 1.1 \
+--dof_hi_sigma None \
+--canny_sigma 1.4 \
+--lo_len_lim_pop1 1.5 \
+--hi_len_lim_pop1 3 \
+--lo_len_lim_pop2 3.4 \
+--hi_len_lim_pop2 4.5
