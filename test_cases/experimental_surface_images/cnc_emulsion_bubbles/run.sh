09ad8c11ef16dedb0ac0214d1c1071c178160852
Andre Guerra <andre.guerra@mail.mcgill.ca>
2025-04-26 18:57:32 -0400

    added run.sh for bubble emulsions

---
+++ test_cases/experimental_surface_images/cnc_emulsion_bubbles/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python src/characteristic_length.py \
+--file 30_-S-In-H-Batch1-Day7-1.jpg \
+--dconv True \
+--pop_num 2 \
+--bar_len 50 \
+--dof_lo_sigma 3.0 \
+--dof_hi_sigma 12 \
+--canny_sigma 1.4 \
+--lo_len_lim_pop1 0.7 \
+--hi_len_lim_pop1 1.6 \
+--lo_len_lim_pop2 0.11 \
+--hi_len_lim_pop2 0.45
